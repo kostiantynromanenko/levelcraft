3652bd2a82c733662b1aefc59a7129090aa52b75
Konstantin Romanenko <kostiantyn.romanenko@gmail.com>
2025-01-13 21:32:08 +0200

    feat: added on click navigation

--- scripts/characters/playerOnGlobalMap.gd
+++ scripts/characters/playerOnGlobalMap.gd
@@ -5,21 +5,28 @@ extends CharacterBody2D
 @onready var road_map: TileMapLayer = get_parent().get_node("RoadsLayer")
 
 const SPEED = 200
+const MOUSE_BUTTON_LEFT = 1
 
 var astar = AStar2D.new()
-var target_position: Vector2
+var path = []
+var current_path_index = 0
 var is_moving = false
 var can_move = true
+var target_position: Vector2
 
 var entrances_data = preload("res://scripts/map/entrances.gd").new()
 
 func _ready() -> void:
 	setup_animation()
 	setup_move_timer()
+	populate_astar()
 
 func _process(delta: float) -> void:
 	if is_moving:
-		process_movement(delta)
+		if path.size() > 0:
+			process_path_movement(delta)
+		else:
+			process_keyboard_movement(delta)
 	else:
 		handle_input()
 
@@ -53,12 +60,13 @@ func handle_manual_entrance_check() -> void:
 func handle_mouse_click(mouse_position: Vector2) -> void:
 	var local_position = road_map.to_local(mouse_position)
 	var clicked_cell = road_map.local_to_map(local_position)
+	var current_cell = road_map.local_to_map(position)
 
 	if check_for_entrance(clicked_cell):
 		return
 
-	if is_valid_move(clicked_cell):
-		start_movement(clicked_cell)
+	if astar.has_point(cell_to_id(clicked_cell)):
+		calculate_path(current_cell, clicked_cell)
 
 func get_direction_from_input() -> Vector2i:
 	var direction = Vector2i.ZERO
@@ -81,27 +89,68 @@ func attempt_move_with_keys(direction: Vector2i) -> void:
 	var target_cell = current_cell + direction
 
 	if is_valid_move(target_cell):
-		start_movement(target_cell)
+		target_position = road_map.map_to_local(target_cell)
+		start_keyboard_movement(target_cell)
+
+func populate_astar() -> void:
+	var used_cells = road_map.get_used_cells()
+
+	for cell in used_cells:
+		var id = cell_to_id(cell)
+		astar.add_point(id, cell)
+
+		for neighbor_offset in [Vector2i(-1, 0), Vector2i(1, 0), Vector2i(0, -1), Vector2i(0, 1)]:
+			var neighbor = cell + neighbor_offset
+			if neighbor in used_cells:
+				var neighbor_id = cell_to_id(neighbor)
+				if astar.has_point(neighbor_id):
+					astar.connect_points(id, neighbor_id)
+
+func is_walkable(cell: Vector2i) -> bool:
+	return road_map.get_cell_source_id(cell) != -1
+
+func cell_to_id(cell: Vector2i) -> int:
+	return cell.x + cell.y * road_map.get_used_rect().size.x
+
+func calculate_path(start_cell: Vector2i, target_cell: Vector2i) -> void:
+	path = astar.get_point_path(cell_to_id(start_cell), cell_to_id(target_cell))
+	current_path_index = 0
+	if path.size() > 0:
+		start_path_movement()
+
+func start_path_movement() -> void:
+	is_moving = true
+	animated_sprite.play("walk")
+
+func process_path_movement(delta: float) -> void:
+	if current_path_index < path.size():
+		target_position = road_map.map_to_local(path[current_path_index])
+		position = position.move_toward(target_position, SPEED * delta)
+
+		if position.distance_to(target_position) < 1:
+			current_path_index += 1
+	else:
+		path.clear()
+		is_moving = false
+		animated_sprite.play("idle")
 
 func is_valid_move(cell: Vector2i) -> bool:
 	return road_map.get_cell_source_id(cell) != -1
 
-func start_movement(target_cell: Vector2i) -> void:
+func start_keyboard_movement(target_cell: Vector2i) -> void:
 	target_position = road_map.map_to_local(target_cell)
 	is_moving = true
 	can_move = false
 	move_timer.start()
 	animated_sprite.play("walk")
-	print("Starting movement to:", target_cell)
 
-func process_movement(delta: float) -> void:
+func process_keyboard_movement(delta: float) -> void:
 	position = position.move_toward(target_position, SPEED * delta)
 
 	if position.distance_to(target_position) < 1:
 		position = target_position
 		is_moving = false
 		animated_sprite.play("idle")
-		print("Movement completed")
 
 func check_for_entrance(cell: Vector2i) -> bool:
 	if cell in entrances_data.entrances:
@@ -110,9 +159,7 @@ func check_for_entrance(cell: Vector2i) -> bool:
 	return false
 
 func handle_entrance(destination_scene: String) -> void:
-	print("Entering location:", destination_scene)
 	get_tree().change_scene_to_file(destination_scene)
 
 func _on_move_timer_timeout() -> void:
 	can_move = true
-	print("Timer expired, movement unlocked")
